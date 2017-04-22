b938df1cf5132c242c30146aa4d70b0cd08b2dee
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-22 19:33:43 +0530

    Added a new main

---
+++ src/mainhome.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ramzan Dieze
+ * Date: 22/04/2017
+ * Time: 19:05
+ */
+
+echo "<h1> Welcome to workedUP!</h1>";
+
+echo "<p>Welcome to workedUP, an amzing place to furnish your home and/or office
+            with unique  stylish products offered at a very competitive price";
+
+echo "<hr/>";
+
+echo "Administrators : <a href= 'adminlogin.php'>Admin Login</a>";
+echo "workedUp friends : <a href= 'index.php'>Go to our Index page</a>";
+
+?>
