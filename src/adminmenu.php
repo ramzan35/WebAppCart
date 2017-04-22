a14ceef6583e8f4ed05d4f3fe58d72829e933d06
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-22 20:42:37 +0530

    Added administration control

---
+++ src/adminmenu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ramzan Dieze
+ * Date: 22/04/2017
+ * Time: 19:23
+ */
+include("adminmenu.html");
+
+echo "<h1>Admin Menu</h1>";
+echo "<p>This is your menu, select from one of the options above on the navigation bar.</p>";
+
+?>
